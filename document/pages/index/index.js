83befbbaf7e5e21785be07732ab2daf8f7853074
HaruTheKing <ziyiliu525@gmail.com>
2021-01-15 17:30:32 +0800

    登录基本功能

--- document/pages/index/index.js
+++ document/pages/index/index.js
@@ -2,9 +2,15 @@
 // 获取应用实例
 const app = getApp();
 
+var data = "1";
+
 Page({
   data: {
-    current: "homePage",
+    current: "mine",
+    avatarUrl: "",
+    userInfo: "",
+    hasUserInfo: false,
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
   },
   handleChange: function ({ detail }) {
     // var detail = _a.detail;
@@ -20,19 +26,44 @@ Page({
       current: detail.key,
     });
   },
-  getData() {
-    wx.request({
-      url: "http://api_devs.wanxikeji.cn/api/goodType",
-      data: {
-        x: "",
-        y: "",
-      },
-      header: {
-        "content-type": "application/json", // 默认值
-      },
-      success(res) {
-        console.log(res.data.data);
-      },
+  onLoad() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+      });
+    } else if (this.data.canIUse) {
+      app.userInfoReadyCallback = (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        });
+      };
+    } else {
+      wx.getUserInfo({
+        success: (res) => {
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+          });
+        },
+      });
+    }
+  },
+  getUserInfo(e) {
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
     });
+    // console.log(this.data.userInfo);
+  },
+  logOut() {
+    this.setData({
+      userInfo: {},
+      hasUserInfo: false,
+    });
+    // app.globalData.userInfo = false;
   },
 });
